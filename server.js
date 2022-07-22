075460b0c29b9494447004a8f74464e26d8b7f20
unknown <wsite079@gmail.com>
2022-07-22 22:57:02 +0600

    upload code

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+
+var express = require("express")
+var app = express()
+var PORT = 8888 || process.env.PORT
+var http = require("http").createServer(app).listen(PORT)
+
 var Turn = require('node-turn');
 var server = new Turn({
   // set options
   authMech: 'long-term',
-  listeningPort:8888 || process.env.PORT,
+  listeningPort:PORT,
   credentials: {
     username: "password",
     
@@ -14,10 +20,15 @@ var server = new Turn({
 server.addUser("admin","password")
 
 
-server.addListener("connect",(ev)=>{
-  console.log("connect")
-})
 
+app.get("/",(req,res)=>{
+  res.send(`
+  <h1>Public Turn server</h1>
+  <h3>name : admin</h3>
+  <h3> Password : password</h3>
+  <h3>PORT : ${PORT} </h3>
+  `)
+})
 
 
 
